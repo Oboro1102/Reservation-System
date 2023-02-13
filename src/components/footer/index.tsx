3ccb75d3c3866430beee3da8df9121c8392c2e88
Oboro <oboro1102@gmail.com>
2023-02-13 18:23:58 +0800

    修正版權文字

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="w-full flex items-center justify-between flex-wrap p-4 text-xs text-white bg-blue-600">
       <span>本系統採用 React + Redux 進行前端開發。</span>
-      <span>&copy; 2022{<span>{currentYear !== 2022 && `&nbsp;-&nbsp;${currentYear}`}</span>}&nbsp;Design & Coding by ツキノリュウ.</span>
+      <span>&copy; 2022{<span>{currentYear !== 2022 && ` - ${currentYear}`}</span>}&nbsp;Design & Coding by ツキノリュウ.</span>
     </footer>
   );
 }
